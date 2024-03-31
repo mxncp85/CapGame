15df99b3c211c7be3ce96d2ea90ff6c79e0a1b35
K4OS <maxence.perronie@epitech.eu>
2024-03-31 23:38:27 +0200

    Ajout de la lib Soil2 et de premake

--- libs.h
+++ libs.h
@@ -1,4 +1,20 @@
 #pragma once
 
+/*OpenGL libs*/
+#include <GL/glew.h> /*You must always include glew before glfw*/
 #include <GLFW/glfw3.h>
-#include <GL/glew.h>
+
+/*Maths imports*/
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+/*Include Soil2*/
+#include <SOIL2.h>
+
+/*Other includes*/
+#include <iostream>
--- main.cpp
+++ main.cpp
@@ -2,36 +2,45 @@
 
 int main(void)
 {
-    GLFWwindow* window;
+    glfwInit();
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    //Create the window here
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
         glfwTerminate();
-        return -1;
+        throw "[ERROR]GlewInit: not initialised.";
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    system("PAUSE");
+        
     return 0;
 }
+
+
+// GLFWwindow* window;
+///* Create a windowed mode window and its OpenGL context */
+//window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//if (!window)
+//{
+//    glfwTerminate();
+//    return -1;
+//}
+
+///* Make the window's context current */
+//glfwMakeContextCurrent(window);
+
+///* Loop until the user closes the window */
+//while (!glfwWindowShouldClose(window))
+//{
+//    /* Render here */
+//    glClear(GL_COLOR_BUFFER_BIT);
+
+//    /* Swap front and back buffers */
+//    glfwSwapBuffers(window);
+
+//    /* Poll for and process events */
+//    glfwPollEvents();
+//}
+
+//glfwTerminate();
